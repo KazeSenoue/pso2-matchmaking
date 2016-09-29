409c57c39c4e3cc3db863a8e69df7357fe4b46c2
Kaze Senoue <kazesenoue@gmail.com>
2016-09-28 21:30:48 -0300

    Changing shit

--- cogs/matchmaking.py
+++ cogs/matchmaking.py
@@ -26,7 +26,7 @@ class Matchmaking:
                 WriteFile('cogs/json/users.json', file)
 
                 await self.bot.change_nickname(ctx.message.author, playerID)
-                await self.bot.add_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name="Member"))
+                await self.bot.add_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name="LFP"))
 
     @commands.command(pass_context=True)
     async def changePID(self, ctx, *, playerID: str):
@@ -46,98 +46,94 @@ class Matchmaking:
         """Creates a group."""
 
         with self.lock:
-            if ctx.message.server.id == "228244312041848833":
-                file = ReadFile('cogs/json/matchmaking.json')
-                users = ReadFile('cogs/json/users.json')
+            file = ReadFile('cogs/json/matchmaking.json')
+            users = ReadFile('cogs/json/users.json')
 
-                try:
-                    id = file['groups'][-1]['id'] + 1
-                except Exception:
-                    id = 0
+            try:
+                id = file['groups'][-1]['id'] + 1
+            except Exception:
+                id = 0
 
-                file['groups'].append({"id" : id, "quest" : questname, "ship" : ship, "owner" : ctx.message.author.id, "members" : [ctx.message.author.id], "maxmembers" : maxmembers})
-                if not discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)):
-                    await self.bot.create_role(ctx.message.server, name="Group {}".format(id))
+            file['groups'].append({"id" : id, "quest" : questname, "ship" : ship, "owner" : ctx.message.author.id, "members" : [ctx.message.author.id], "maxmembers" : maxmembers})
+            if not discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)):
+                await self.bot.create_role(ctx.message.server, name="Group {}".format(id))
 
-                await asyncio.sleep(1)
-                await self.bot.add_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)))
+            await asyncio.sleep(1)
+            await self.bot.add_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)))
 
-                everyone = discord.PermissionOverwrite(read_messages=False)
-                mine = discord.PermissionOverwrite(read_messages=True, send_messages=True)
+            everyone = discord.PermissionOverwrite(read_messages=False)
+            mine = discord.PermissionOverwrite(read_messages=True, send_messages=True)
 
-                await self.bot.create_channel(ctx.message.server, 'group-{}'.format(id), (ctx.message.server.default_role, everyone), (discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)), mine))
+            await self.bot.create_channel(ctx.message.server, 'group-{}'.format(id), (ctx.message.server.default_role, everyone), (discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)), mine))
 
-                WriteFile('cogs/json/matchmaking.json', file)
+            WriteFile('cogs/json/matchmaking.json', file)
 
-                message = "@here A group for ``{}`` on Ship {} has been created by ``{}``. Type ``!lfp join {}`` to join!".format(questname, ship, ctx.message.author.nick, id)
-                await self.bot.send_message(discord.Object("228986443920310274"), message)
+            message = "@here A group for ``{}`` on Ship {} has been created by ``{}``. Type ``!lfp join {}`` to join!".format(questname, ship, ctx.message.author.nick, id)
+            await self.bot.send_message(discord.Object("228986443920310274"), message)
 
     @lfp.command(pass_context=True)
     async def join(self, ctx, id : int):
         """Joins a group."""
 
         with self.lock:
-            if ctx.message.server.id == "228244312041848833":
-                try:
-                    users = ReadFile('cogs/json/users.json')
-                    file = ReadFile('cogs/json/matchmaking.json')
-                    index = SearchGroup(id)
+            try:
+                users = ReadFile('cogs/json/users.json')
+                file = ReadFile('cogs/json/matchmaking.json')
+                index = SearchGroup(id)
 
-                    if ctx.message.author.id not in file['groups'][index]['members']:
-                        try:
-                            await self.bot.add_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)))
-                            file['groups'][index]['members'].append(ctx.message.author.id)
-                            WriteFile('cogs/json/matchmaking.json', file)
+                if ctx.message.author.id not in file['groups'][index]['members']:
+                    try:
+                        await self.bot.add_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)))
+                        file['groups'][index]['members'].append(ctx.message.author.id)
+                        WriteFile('cogs/json/matchmaking.json', file)
 
-                            await self.bot.say("{} Joined the ``{}`` group, owned by ``{}``.".format(ctx.message.author.mention, file['groups'][index]['quest'], users[file['groups'][index]['owner']]))
+                        await self.bot.say("{} Joined the ``{}`` group, owned by ``{}``.".format(ctx.message.author.mention, file['groups'][index]['quest'], users[file['groups'][index]['owner']]))
 
-                        except Exception as e:
-                            await self.bot.say("{} Could not find the requested group. Please check the #group-board channel.".format(ctx.message.author.mention))
-                    else:
-                        await self.bot.say("{} You are already in that group.".format(ctx.message.author.mention))
-                except:
-                    await self.bot.say("{} Could not find a group with that ID. Check #groups-board for the right ID.".format(ctx.message.author.mention))
+                    except Exception as e:
+                        await self.bot.say("{} Could not find the requested group. Please check the #group-board channel.".format(ctx.message.author.mention))
+                else:
+                    await self.bot.say("{} You are already in that group.".format(ctx.message.author.mention))
+            except:
+                await self.bot.say("{} Could not find a group with that ID. Check #groups-board for the right ID.".format(ctx.message.author.mention))
 
     @lfp.command(pass_context=True)
     async def leave(self, ctx, id : int):
         """Leaves the selected group."""
 
         with self.lock:
-            if ctx.message.server.id == "228244312041848833":
-                file = ReadFile('cogs/json/matchmaking.json')
-                index = SearchGroup(id)
+            file = ReadFile('cogs/json/matchmaking.json')
+            index = SearchGroup(id)
 
-                if ctx.message.author.id in file['groups'][index]['members']:
-                    await self.bot.remove_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)))
+            if ctx.message.author.id in file['groups'][index]['members']:
+                await self.bot.remove_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)))
 
-                    file['groups'][index]['members'].remove(ctx.message.author.id)
+                file['groups'][index]['members'].remove(ctx.message.author.id)
 
-                    WriteFile('cogs/json/matchmaking.json', file)
+                WriteFile('cogs/json/matchmaking.json', file)
 
-                    await self.bot.say("{} You have been removed from the ``{}`` group.".format(ctx.message.author.mention, file['groups'][id]['quest']))
-                else:
-                    await self.bot.say("{} You are not in that group.".format(ctx.message.author.mention))
+                await self.bot.say("{} You have been removed from the ``{}`` group.".format(ctx.message.author.mention, file['groups'][id]['quest']))
+            else:
+                await self.bot.say("{} You are not in that group.".format(ctx.message.author.mention))
 
     @lfp.command(pass_context=True)
     async def remove(self, ctx, id : int, member : str):
         """Removes a selected member from a group. GROUP OWNER ONLY"""
 
         with self.lock:
-            if ctx.message.server.id == "228244312041848833":
-                file = ReadFile('cogs/json/matchmaking.json')
-                index = SearchGroup(id)
+            file = ReadFile('cogs/json/matchmaking.json')
+            index = SearchGroup(id)
 
-                if ctx.message.author.id == file['groups'][index]['owner']:
-                    try:
-                        await self.bot.remove_roles(ctx.message.server.get_member(member), discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)))
-                        file['groups'][index]['members'].remove(member)
-                        WriteFile('cogs/json/matchmaking.json', file)
+            if ctx.message.author.id == file['groups'][index]['owner']:
+                try:
+                    await self.bot.remove_roles(ctx.message.server.get_member(member), discord.utils.get(ctx.message.server.roles, name="Group {}".format(id)))
+                    file['groups'][index]['members'].remove(member)
+                    WriteFile('cogs/json/matchmaking.json', file)
 
-                        await self.bot.say("{} Member ``{}`` removed from the ``{}`` group.".format(ctx.message.author.mention, member, file['groups'][index]['quest']))
-                    except Exception as e:
-                        await self.bot.say("{} Member ``{}`` not found in the ``{}`` group.".format(ctx.message.author.mention, member, file['groups'][index]['quest']))
-                else:
-                    await self.bot.say("{} You are not the owner of the group.".format(ctx.message.author.mention))
+                    await self.bot.say("{} Member ``{}`` removed from the ``{}`` group.".format(ctx.message.author.mention, member, file['groups'][index]['quest']))
+                except Exception as e:
+                    await self.bot.say("{} Member ``{}`` not found in the ``{}`` group.".format(ctx.message.author.mention, member, file['groups'][index]['quest']))
+            else:
+                await self.bot.say("{} You are not the owner of the group.".format(ctx.message.author.mention))
 
 def setup(bot):
     bot.add_cog(Matchmaking(bot))
